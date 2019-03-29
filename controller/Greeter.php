328db5436e89df4fd5115720713272ed3af9ce7e
Shadi-Ayoub <shadi_ayoub@yahoo.com>
2019-03-29 09:50:49 +0400

    more updates!

--- controller/Greeter.php
+++ controller/Greeter.php
@@ -8,10 +8,63 @@ class Greeter extends \tao_actions_CommonModule {
      */
     public function __construct() {
         parent::__construct();
+        
+        // Data for our example extension:
+        $this->pilots = [
+            'red_1' => 'luke',
+            'red_2' => 'biggs',
+            'red_3' => 'wedge',
+        ];
     }
     
     public function helloFromTemplate() {
         $this->setData('author', 'Shadi Ayoub');
         $this->setView('Greeter/myName.tpl');
+    }
+    
+    public function getPilotsList() {
+
+        $data = array(
+            'data'  => __("Pilots"),
+            'attributes' => array(
+                    'id' => 1,
+                    'class' => 'node-class'
+                ),
+            'children' => array()
+            );
+
+        foreach ($this->pilots as $index => $name) {
+            $data['children'][] =  array(
+                'data'  => 'my name is ' . ucfirst($name),
+                'attributes' => array(
+                        'id' => $index,
+                        'class' => 'node-instance'
+                    )
+            );
+        }
+        echo json_encode($data);
+    }
+    
+    public function vader() {
+        $name = '';
+        if($this->hasRequestParameter('uri')) {
+            $uri = $this->getRequestParameter('uri');
+            if(array_key_exists($uri, $this->pilots)) {
+                $name = $this->pilots[$uri];
+            }
+        }
+        echo ucfirst($name) . ', I\'m your father';
+
+    }
+
+    public function obiwan() {
+        $name = '';
+        if($this->hasRequestParameter('uri')) {
+            $uri = $this->getRequestParameter('uri');
+            if(array_key_exists($uri, $this->pilots)) {
+                $name = $this->pilots[$uri];
+            }
+        }
+        echo ucfirst($name) . ', may the force be with you';
     }
 }
